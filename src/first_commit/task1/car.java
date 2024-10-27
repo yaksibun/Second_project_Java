412098f77522980ca63f4077b7defca63f25a401
Yaksibun <abdtemur02@gmail.com>
2024-10-27 19:19:31 +0500

    chhgh

---
+++ src/first_commit/task1/car.java
@@ -0,0 +1,21 @@
+package first_commit.task1;
+
+public class car {
+
+    public void getColor(String color){
+        System.out.println("Color: "+color);
+    }
+
+    public void getModel(String model){
+        System.out.println("Model: "+model);
+    }
+
+    public void getPrice(String price){
+        System.out.println("Price: "+price);
+    }
+
+    public void getManage(String manage) {
+        System.out.println("Management: " + manage);
+    }
+}
+
