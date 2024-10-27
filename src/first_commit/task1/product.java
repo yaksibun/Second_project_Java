b24e55472d7b606c383c9df5054326a737faa21a
Yaksibun <abdtemur02@gmail.com>
2024-10-27 18:02:14 +0500

    chhgh

--- src/first_commit/task1/product.java
+++ src/first_commit/task1/product.java
@@ -1,18 +1,18 @@
-//package first_commit.task1;
-//
-//public class product
-//{
-//    public static void main(String[] args) {
-//        car bmw = new car();
-//        bmw.getModel("BMW X5");
-//        bmw.getColor("Black");
-//        bmw.getManage("Auto");
-//        bmw.getPrice("$1700");
-//
-//        car mers = new car();
-//        mers.getModel("GLC 500");
-//        mers.getColor("White");
-//        mers.getManage("Mexanik");
-//        mers.getPrice("$15000");
-//    }
-//}
+package first_commit.task1;
+
+public class product
+{
+    public static void main(String[] args) {
+        car bmw = new car();
+        bmw.getModel("BMW X5");
+        bmw.getColor("Black");
+        bmw.getManage("Auto");
+        bmw.getPrice("$1700");
+
+        car mers = new car();
+        mers.getModel("GLC 500");
+        mers.getColor("White");
+        mers.getManage("Mexanik");
+        mers.getPrice("$15000");
+    }
+}
